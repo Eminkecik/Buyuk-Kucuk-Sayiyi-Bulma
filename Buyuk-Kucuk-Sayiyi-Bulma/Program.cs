71e1c629696f5d2d4570f11edfa9b2ab44beb2ee
Emin Keçik <Eminkecik8@gmail.com>
2019-12-31 11:14:57 +0300

    Add project files.

---
+++ Buyuk-Kucuk-Sayiyi-Bulma/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buyuk_Kucuk_Sayiyi_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
