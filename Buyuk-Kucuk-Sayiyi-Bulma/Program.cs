54f1d26da9d93fda6757068853c3259bc8442340
Emin Keçik <Eminkecik8@gmail.com>
2019-12-31 11:18:47 +0300

    Buyuk-Kucuk-Sayiyi-Bulma

--- Buyuk-Kucuk-Sayiyi-Bulma/Program.cs
+++ Buyuk-Kucuk-Sayiyi-Bulma/Program.cs
@@ -10,6 +10,29 @@ namespace Buyuk_Kucuk_Sayiyi_Bulma
     {
         static void Main(string[] args)
         {
+            int[] sayilar = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write((i + 1).ToString() + ". Sayıyı giriniz: ");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int enbuyuk, enkucuk;
+            enkucuk = sayilar[0];
+            enbuyuk = sayilar[0];
+            for (int i = 1; i < 4; i++)
+            {
+                if (enbuyuk < sayilar[i])
+                {
+                    enbuyuk = sayilar[i];
+                }
+                else if (enkucuk > sayilar[i])
+                {
+                    enkucuk = sayilar[i];
+                }
+            }
+            Console.WriteLine("En küçük sayı: " + enkucuk);
+            Console.WriteLine("En büyük sayı: " + enbuyuk);
+            Console.ReadLine();
         }
     }
 }
